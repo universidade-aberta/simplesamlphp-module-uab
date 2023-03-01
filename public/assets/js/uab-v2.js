19463881c7da99bcf19cf5a47f9f393bae34c51f
Cláudio Esperança <cesperanc@gmail.com>
2023-03-01 09:43:33 +0000

    Add some more randomness to the background animation;
    Signed-off-by: Cláudio Esperança <cesperanc@gmail.com>

--- public/assets/js/uab-v2.js
+++ public/assets/js/uab-v2.js
@@ -12,5 +12,9 @@ document.addEventListener("DOMContentLoaded", ()=>{
         const currentAnimationDuration = getComputedStyle(el).animationDuration??'60s';
         const randomDelay = Math.round(Math.random()*100)/100;
         el.style.setProperty("--animation-delay", `calc(${currentAnimationDuration} * -${randomDelay})`);
+        const randomRotation = Math.round(Math.random()*360);
+        el.style.setProperty("--initial-rotation", `${randomRotation}deg`);
+        const scaleFactor = Math.round(Math.random()*3)+1;
+        el.style.setProperty("--scale-multiplier", `${scaleFactor}`);
     });
 });
